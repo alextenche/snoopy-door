1b9b58db196b0bdf0d5a04746390cd858ca7f5b5
Alexandru Tenche <alex.tenche@gmail.com>
2020-03-21 12:45:49 +0200

    updated test for bark recognizer

--- src/test/java/DogDoorSimulator.java
+++ src/test/java/DogDoorSimulator.java
@@ -3,19 +3,34 @@ public class DogDoorSimulator {
     public static void main(String[] args) {
 
         DogDoor door = new DogDoor();
-        Remote remote = new Remote(door);
+//        Remote remote = new Remote(door);
+        BarkRecognizer recognizer = new BarkRecognizer(door);
 
         System.out.println("Fido barks to go outside...");
-        remote.pressButton();
+
+        System.out.println("door bark recognizer in action");
+        recognizer.recognize("woof");
+//        remote.pressButton();
 
         System.out.println("\nFido has gone outside...");
-        remote.pressButton();
-
         System.out.println("\nFido’s all done...");
-        remote.pressButton();
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            System.out.println("problem with timer");
+        }
+
+        System.out.println("...but he's stuck outside!");
+        System.out.println("door bark recognizer in action again ");
+        recognizer.recognize("woof woof");
+
+//        System.out.println("...so Todd grabs the remote control.");
+//        remote.pressButton();
+
 
         System.out.println("\nFido’s back inside...");
-        remote.pressButton();
+//        remote.pressButton();
 
     }
 
