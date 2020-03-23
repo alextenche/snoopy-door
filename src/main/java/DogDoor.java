ba0dc7ea5227b2b041628081b8cc7bebe840a4e8
Alexandru Tenche <alex.tenche@gmail.com>
2020-03-23 22:43:17 +0200

    added multiple barks, timer

--- src/main/java/DogDoor.java
+++ src/main/java/DogDoor.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class DogDoor {
 
     private boolean open;
+    private List<Bark> allowedBarks = new ArrayList<Bark>();
 
     public DogDoor() {
         this.open = false;
@@ -9,6 +15,13 @@ public class DogDoor {
     public void open() {
         System.out.println("The dog door opens.");
         open = true;
+        final Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            public void run() {
+                close();
+                timer.cancel();
+            }
+        }, 5000);
     }
 
     public void close() {
@@ -20,4 +33,11 @@ public class DogDoor {
         return open;
     }
 
+    public List<Bark> getAllowedBarks() {
+        return allowedBarks;
+    }
+
+    public void addAllowedBark(Bark bark) {
+        allowedBarks.add(bark);
+    }
 }
