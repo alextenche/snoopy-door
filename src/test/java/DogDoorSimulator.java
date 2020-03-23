d3587f4f98b0e443b5fa72b29c83b12ea77bf3f3
Alexandru Tenche <alex.tenche@gmail.com>
2020-03-23 22:35:53 +0200

    changes in test for barkrecognizer

--- src/test/java/DogDoorSimulator.java
+++ src/test/java/DogDoorSimulator.java
@@ -3,17 +3,19 @@ public class DogDoorSimulator {
     public static void main(String[] args) {
 
         DogDoor door = new DogDoor();
-//        Remote remote = new Remote(door);
+
+        door.addAllowedBark(new Bark("rowlf"));
+        door.addAllowedBark(new Bark("rooowlf"));
+        door.addAllowedBark(new Bark("rawlf"));
+        door.addAllowedBark(new Bark("woof"));
         BarkRecognizer recognizer = new BarkRecognizer(door);
+        Remote remote = new Remote(door);
 
-        System.out.println("Fido barks to go outside...");
+        // Simulate the hardware hearing a bark
+        System.out.println("Bruce starts barking.");
+        recognizer.recognize(new Bark("rowlf"));
 
-        System.out.println("door bark recognizer in action");
-        recognizer.recognize("woof");
-//        remote.pressButton();
-
-        System.out.println("\nFido has gone outside...");
-        System.out.println("\nFido’s all done...");
+        System.out.println("Bruce has gone outside...");
 
         try {
             Thread.sleep(10000);
@@ -21,16 +23,25 @@ public class DogDoorSimulator {
             System.out.println("problem with timer");
         }
 
+        System.out.println("Bruce’s all done...");
         System.out.println("...but he's stuck outside!");
-        System.out.println("door bark recognizer in action again ");
-        recognizer.recognize("woof woof");
 
-//        System.out.println("...so Todd grabs the remote control.");
-//        remote.pressButton();
+        // Simulate the hardware hearing a bark (not Bruce!)
+        Bark smallDogBark = new Bark("yip");
+        System.out.println("A small dog starts barking.");
+        recognizer.recognize(smallDogBark);
 
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            System.out.println("problem with timer");
+        }
 
-        System.out.println("\nFido’s back inside...");
-//        remote.pressButton();
+        // Simulate the hardware hearing a bark again
+        System.out.println("Bruce starts barking.");
+        recognizer.recognize(new Bark("rooowlf"));
+
+        System.out.println("Bruce’s back inside...");
 
     }
 
